206008b013abfb422ce5c06f085af05d439502df
Orest Dubay <orest3.dubay@gmail.com>
2021-11-27 21:42:32 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  render: h => h(App),
+  data: {
+    status: "OK",
+    message: "",
+    url_submit_prefix: "/liquer/submit/",
+    url_remove_prefix: "/liquer/cache/remove/",
+    url_prefix: "/liquer/q/",
+  }
+}).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+  assetsDir:"liquer/web/gui",
+  indexPath:"liquer/web/gui/index.html"
+}
