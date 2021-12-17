0af7d696e7a5ff3f51fd97cfd55fd89d93ee97c0
Orest Dubay <orest3.dubay@gmail.com>
2021-12-17 21:05:37 +0100

    metadata viewer improvements

--- src/components/MetadataView.vue
+++ src/components/MetadataView.vue
@@ -1,17 +1,39 @@
 <template>
-  <v-card>    
+  <v-card>
     <v-card-title v-if="!has_metadata">
-      No Metadata {{query}}
+      No Metadata {{ query }}
       <v-spacer></v-spacer>
     </v-card-title>
     <v-card-title v-if="has_metadata">
-      Metadata {{query}}
-      <v-spacer></v-spacer>
+      <ResultStatus :status="metadata.status"></ResultStatus> {{ query }}
     </v-card-title>
-    <v-container v-if="has_metadata">
-        <v-row><v-col>Status</v-col><v-col><ResultStatus :status="metadata.status"></ResultStatus></v-col></v-row>
-        <v-row><v-col>Parent</v-col><v-col>{{metadata.parent_query}}</v-col></v-row>
-    </v-container>
+    <v-expansion-panels v-if="has_metadata" v-model="panel" multiple>
+      <v-expansion-panel>
+        <v-expansion-panel-header>Description</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <h3>{{ title }}</h3>
+          <em>{{ data_description }}</em>
+          <br />
+          <pre>{{ description }}</pre>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header>Info</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <v-simple-table>
+            <tbody>
+              <tr><td>Query</td><td>{{query}}</td></tr>
+              <tr><td>Key</td><td>{{key}}</td></tr>
+              <tr><td>Parent query</td><td>{{metadata.parent_query}}</td></tr>
+              <tr><td>Type identifier</td><td>{{metadata.type_identifier}}</td></tr>
+              <tr><td>Started</td><td>{{metadata.started}}</td></tr>
+              <tr><td>Updated</td><td>{{metadata.updated}}</td></tr>
+              <tr><td>Created</td><td>{{metadata.created}}</td></tr>
+            </tbody>
+          </v-simple-table>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
   </v-card>
 </template>
 
@@ -32,6 +54,7 @@ export default {
     },
   },
   data: () => ({
+    panel: [0],
   }),
   methods: {
     info(message, reason = null, query = null) {
@@ -43,34 +66,57 @@ export default {
     message_event(m) {
       this.$emit("message-event", m);
     },
-    update(){
-
-    }
+    update() {},
   },
   computed: {
-      has_metadata(){
-          try{
-              return (("status" in this.metadata) || ("query" in this.metadata));
-          }
-          catch{
-              return false;
-          }
-      },
-      query(){
-          try{
-              return this.metadata.query;
-          }
-          catch{
-              return "-na-";
-          }
+    has_metadata() {
+      try {
+        return "status" in this.metadata || "query" in this.metadata;
+      } catch {
+        return false;
       }
+    },
+    query() {
+      try {
+        return this.metadata.query;
+      } catch {
+        return "-na-";
+      }
+    },
+    title() {
+      try {
+        return this.metadata.title;
+      } catch {
+        return "";
+      }
+    },
+    description() {
+      try {
+        return this.metadata.description;
+      } catch {
+        return "";
+      }
+    },
+    key() {
+      try {
+        return this.metadata.key;
+      } catch {
+        return "";
+      }
+    },
+    data_description() {
+      try {
+        return this.metadata.data_characteristics.description;
+      } catch {
+        return "";
+      }
+    },
   },
   watch: {
     metadata() {
       this.update();
     },
   },
-  created: function () {
-  },
+  created: function () {},
 };
 </script>
